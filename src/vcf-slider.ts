c04ac019d2416cad9ac6ffcbd92e028794dc7264
Nii Yeboah <nii@vaadin.com>
2023-01-03 18:55:30 +0800

    Refactor tooltip styles Add tooltips-always-visible property Add dragging attr and active classes

--- src/vcf-slider.ts
+++ src/vcf-slider.ts
@@ -7,15 +7,14 @@ import { ThemableMixin } from '@vaadin/vaadin-themable-mixin';
  * `<vcf-slider>` Slider web component for the Vaadin platform.
  *
  * @csspart container - Wrapper element.
- * @csspart line - Line element.
  * @csspart knob - Knob elements.
  * @csspart knob-n - Nth knob element.
  * @csspart tooltip - Knob tooltip containers.
  * @csspart tooltip-n - Nth knob tooltip container.
- * @csspart tooltip-value - Knob tooltip value elements.
- * @csspart tooltip-value-n - Nth knob tooltip value element.
  * @csspart tooltip-triangle - Knob tooltip triangle elements.
  * @csspart tooltip-triangle-n - Nth knob tooltip triangle element.
+ * @csspart tooltip-value - Knob tooltip value elements.
+ * @csspart tooltip-value-n - Nth knob tooltip value element.
  *
  * @cssprop [--vcf-slider-knob-alt-color=var(--lumo-error-color)] - Color of `::part(alt-knob)`.
  * @cssprop [--vcf-slider-knob-color=var(--lumo-primary-color)] - Color of `::part(knob)`.
@@ -31,8 +30,11 @@ import { ThemableMixin } from '@vaadin/vaadin-themable-mixin';
  */
 @customElement('vcf-slider')
 export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
-  /** If `true`, show tooltips that display values above slider knobs. */
+  /** If `true`, show tooltips that display values above slider knobs when active. */
   @property({ type: Boolean, reflect: true }) tooltips = false;
+
+  /** If `true`, always show tooltips above slider knobs. */
+  @property({ type: Boolean, reflect: true, attribute: 'tooltips-always-visible' }) tooltipsAlwaysVisible = false;
 
   /** If `true`, change *orientation* of the range slider from horizontal to vertical. */
   @property({ type: Boolean, reflect: true }) vertical = false;
@@ -56,8 +58,7 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
   @property({ type: Number }) max = 100;
 
   @query('#knobs') private knobsContainer?: HTMLElement;
-  @query('#line') private line?: HTMLElement;
-  @query('#line-color') private lineColorElement?: HTMLElement;
+  @query('#container') private container?: HTMLElement;
 
   protected static is() {
     return 'vcf-slider';
@@ -72,6 +73,9 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
   private originalPointerXY: number | null = 0;
   private knobCount = 1;
   private decimalCount = 0;
+  private tooltipActiveTimeout?: number = 0;
+  private passiveHandleEvent = this.eventListenerObject();
+  private handleEvent = this.eventListenerObject(false);
 
   private get xy() {
     return this.vertical ? 'y' : 'x';
@@ -94,8 +98,11 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
       :host {
         display: flex;
         margin: var(--lumo-space-s) 0;
+        padding: var(--vcf-slider-padding);
         width: var(--vcf-slider-width);
-        /* PUBLIC */
+        box-sizing: border-box;
+        outline: none;
+        /* PUBLIC PROPERTIES */
         --vcf-slider-knob-alt-color: var(--lumo-error-color);
         --vcf-slider-knob-color: var(--lumo-primary-color);
         --vcf-slider-knob-size: var(--lumo-space-m);
@@ -106,7 +113,7 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
         --vcf-slider-padding: var(--lumo-space-xs);
         --vcf-slider-width: 100%;
         --vcf-slider-vertical-height: 200px;
-        /* PRIVATE */
+        /* PRIVATE PROPERTIES */
         --l-height: var(--vcf-slider-line-height);
         --k-size: calc(var(--vcf-slider-knob-size) * 2);
       }
@@ -115,16 +122,11 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
         box-sizing: border-box;
       }
 
-      :host([tooltips]) {
+      :host([tooltips][tooltips-always-visible]) {
         padding-top: calc(var(--vcf-slider-tooltip-font-size) + 4px + var(--lumo-space-s));
       }
 
       #container {
-        width: 100%;
-        padding: var(--vcf-slider-padding);
-      }
-
-      #line {
         position: relative;
         width: 100%;
         height: var(--l-height);
@@ -151,6 +153,13 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
         width: var(--k-size);
         height: var(--k-size);
         user-select: none;
+        filter: brightness(1);
+      }
+
+      [part~='knob']:hover,
+      [part~='knob'].active {
+        filter: brightness(1.1);
+        transition: filter 0.3s;
       }
 
       [part~='knob']::after {
@@ -169,6 +178,15 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
 
       /* TOOLTIPS */
 
+      :host([tooltips-always-visible]) [part~='tooltip'],
+      :host(:not([tooltips-always-visible])) [part~='tooltip'].active {
+        opacity: 1;
+      }
+
+      :host([tooltips]) [part~='tooltip'] {
+        display: flex;
+      }
+
       [part~='tooltip'] {
         display: none;
         flex-flow: column;
@@ -180,8 +198,10 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
         background-color: var(--lumo-base-color);
         padding: 2px var(--lumo-space-s);
         font-size: var(--vcf-slider-tooltip-font-size);
+        opacity: 0;
         pointer-events: none;
         user-select: none;
+        transition: opacity 0.3s;
       }
 
       [part~='tooltip-triangle'] {
@@ -205,13 +225,9 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
         box-shadow: -2px 2px 2px 0 var(--lumo-shade-20pct);
       }
 
-      :host([tooltips]) [part~='tooltip'] {
-        display: flex;
-      }
-
       /* VERTICAL */
 
-      :host([tooltips][vertical]) {
+      :host([vertical][tooltips][tooltips-always-visible]) {
         padding-top: 0px;
         padding-right: calc(var(--vcf-slider-tooltips-width) + 4px + var(--lumo-space-xs));
       }
@@ -223,7 +239,7 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
         height: var(--vcf-slider-vertical-height);
       }
 
-      :host([vertical]) #line {
+      :host([vertical]) #container {
         width: var(--l-height);
         height: 100%;
       }
@@ -252,7 +268,7 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
         width: 0px;
         height: 0px;
         box-sizing: border-box;
-        border: 7px solid transparent;
+        border: 7.5px solid transparent;
         border-color: transparent transparent var(--lumo-base-color) var(--lumo-base-color);
         transform-origin: 4px -1px;
         transform: rotate(45deg);
@@ -275,10 +291,7 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
   render() {
     return html`
       <div id="container" part="container">
-        <div id="line" part="line">
-          <div id="line-color" part="line-color"></div>
-          <div id="knobs" part="knobs"></div>
-        </div>
+        <div id="knobs" part="knobs"></div>
       </div>
     `;
   }
@@ -320,16 +333,17 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
     observer.observe(this);
   }
 
-  /** @private */
-  passive = 'true';
+  private eventListenerObject(passive = true) {
+    return { passive, handleEvent: (e: Event) => this.eventListener(e) } as EventListenerObject;
+  }
 
-  /** @private */
-  handleEvent(e: Event) {
+  private eventListener(e: Event) {
+    const knob = e.target as HTMLElement;
     switch (e.type) {
       case 'mousedown':
       case 'touchstart':
         if (!Slider.isValid(e) || this.isKnobClick(e)) return;
-        this.knob?.focus();
+        knob.focus();
         this.startDrag(e);
         this.dragging = true;
         break;
@@ -340,9 +354,22 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
       case 'mouseup':
       case 'touchend':
         this.dragging = false;
+        this.setActive(false);
         break;
       case 'keydown':
         this.keyMove(e, this.knobIndex);
+        break;
+      case 'focus':
+        this.knob = knob;
+        this.setActive(true, true);
+        break;
+      case 'mouseenter':
+        this.knob = knob;
+        this.setActive(true);
+        break;
+      case 'blur':
+      case 'mouseleave':
+        this.setActive(false);
         break;
     }
   }
@@ -436,19 +463,19 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
       this.setAriaValues(i, values);
       this.setTooltipPosition(i, values);
       this.setKnobPostion(i, values);
-      this.setLineColors(values);
+      this.setBackgroundColors(values);
     });
     this.dispatchEvent(this.valueChangedEvent);
   }
 
   private setKnobPostion(i = 0, values = this.initialValue) {
-    const { min, max, lineBounds } = this;
+    const { min, max, containerBounds } = this;
     const knob = this.knobElement(i) as HTMLElement;
     if (knob) {
       const knobBounds = this.getBounds(knob);
       const knobSize = this.vertical ? knobBounds.height : knobBounds.width;
-      const lineSize = this.vertical ? lineBounds.height : lineBounds.width;
-      const position = ((values[i] - min) / (max - min)) * lineSize - knobSize / 2;
+      const containerSize = this.vertical ? containerBounds.height : containerBounds.width;
+      const position = ((values[i] - min) / (max - min)) * containerSize - knobSize / 2;
       this.resetPosition(knob);
       if (this.vertical) knob.style[this.rtl ? 'bottom' : 'top'] = `${position}px`;
       else knob.style[this.rtl ? 'right' : 'left'] = `${position}px`;
@@ -456,13 +483,13 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
   }
 
   private setTooltipPosition(i = 0, values = this.values) {
-    const { min, max, lineBounds } = this;
+    const { min, max, containerBounds } = this;
     const tooltip = this.tooltipElement(i) as HTMLElement;
     if (tooltip) {
       const tooltipBounds = this.getBounds(tooltip);
       const tooltipSize = this.vertical ? tooltipBounds.height : tooltipBounds.width;
-      const lineSize = this.vertical ? lineBounds.height : lineBounds.width;
-      const position = ((values[i] - min) / (max - min)) * lineSize - tooltipSize / 2;
+      const containerSize = this.vertical ? containerBounds.height : containerBounds.width;
+      const position = ((values[i] - min) / (max - min)) * containerSize - tooltipSize / 2;
       this.resetPosition(tooltip);
       if (this.vertical) tooltip.style[this.rtl ? 'bottom' : 'top'] = `${position}px`;
       else tooltip.style[this.rtl ? 'right' : 'left'] = `${position}px`;
@@ -477,8 +504,8 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
     element.style.removeProperty('bottom');
   }
 
-  private setLineColors(values = this.values) {
-    const { knobs, min, max, lineColorElement, vertical, rtl } = this;
+  private setBackgroundColors(values = this.values) {
+    const { knobs, min, max, container, vertical, rtl } = this;
     const length = max - min;
     const lineColor = getComputedStyle(this).getPropertyValue('--vcf-slider-line-color').trim();
     const altLineColor = getComputedStyle(this).getPropertyValue('--vcf-slider-line-alt-color').trim();
@@ -494,13 +521,19 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
       prevStop = stop;
     });
     colors += `transparent ${prevStop} 100%`;
-    if (lineColorElement) lineColorElement.style.background = `linear-gradient(to ${direction}, ${colors})`;
+    if (container) container.style.backgroundImage = `linear-gradient(to ${direction}, ${colors})`;
+  }
+
+  private get dragging() {
+    return typeof this.getAttribute('dragging') === 'string';
   }
 
   private set dragging(state: boolean) {
     const toggleEvent = state ? document.addEventListener : document.removeEventListener;
-    toggleEvent(Slider.hasTouched ? 'touchmove' : 'mousemove', this);
-    toggleEvent(Slider.hasTouched ? 'touchend' : 'mouseup', this);
+    toggleEvent(Slider.hasTouched ? 'touchmove' : 'mousemove', this.passiveHandleEvent);
+    toggleEvent(Slider.hasTouched ? 'touchend' : 'mouseup', this.passiveHandleEvent);
+    if (state) this.setAttribute('dragging', '');
+    else this.removeAttribute('dragging');
   }
 
   private getPointerXY(e: Event) {
@@ -514,11 +547,12 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
     const { knobsContainer, xy } = this;
     this.knob = e.target as HTMLElement;
     this.originalPointerXY = this.getPointerXY(e);
-    this.originalKnobOffsetXY = this.getBounds(this.knob)[xy] - this.lineBounds[xy];
+    this.originalKnobOffsetXY = this.getBounds(this.knob)[xy] - this.containerBounds[xy];
 
     // Move current knob and tooltip to top
     knobsContainer?.appendChild(this.knob);
     knobsContainer?.appendChild(this.tooltip);
+    this.setActive(true);
   };
 
   private drag = (e: Event) => {
@@ -531,14 +565,14 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
       originalKnobOffsetXY,
       originalPointerXY,
       xy,
-      line,
-      lineBounds,
+      container,
+      containerBounds,
     } = this;
 
-    if (knob && line) {
+    if (knob && container) {
       const knobBounds = this.getBounds(knob);
       const knobSize = vertical ? knobBounds.height : knobBounds.width;
-      const lineSize = vertical ? lineBounds.height : lineBounds.width;
+      const lineSize = vertical ? containerBounds.height : containerBounds.width;
       const lineStart = -knobSize / 2;
       const lineEnd = lineSize - knobSize / 2;
       const part = `knob-${i}`;
@@ -549,23 +583,23 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
       switch (part) {
         case 'knob-0': {
           if (knobs > 1) {
-            const toKnob = line.querySelector('[part~="knob-1"]') as HTMLElement;
-            end = this.getBounds(toKnob)[xy] - lineBounds[xy];
+            const toKnob = container.querySelector('[part~="knob-1"]') as HTMLElement;
+            end = this.getBounds(toKnob)[xy] - containerBounds[xy];
           }
           break;
         }
         case `knob-${knobs - 1}`: {
           if (knobs > 1) {
-            const fromKnob = line.querySelector(`[part~="knob-${knobs - 2}"]`) as HTMLElement;
-            start = this.getBounds(fromKnob)[xy] - lineBounds[xy];
+            const fromKnob = container.querySelector(`[part~="knob-${knobs - 2}"]`) as HTMLElement;
+            start = this.getBounds(fromKnob)[xy] - containerBounds[xy];
           }
           break;
         }
         default: {
-          const fromKnob = line.querySelector(`[part~="knob-${i - 1}"]`) as HTMLElement;
-          const toKnob = line.querySelector(`[part~="knob-${i + 1}"]`) as HTMLElement;
-          start = this.getBounds(fromKnob)[xy] - lineBounds[xy];
-          end = this.getBounds(toKnob)[xy] - lineBounds[xy];
+          const fromKnob = container.querySelector(`[part~="knob-${i - 1}"]`) as HTMLElement;
+          const toKnob = container.querySelector(`[part~="knob-${i + 1}"]`) as HTMLElement;
+          start = this.getBounds(fromKnob)[xy] - containerBounds[xy];
+          end = this.getBounds(toKnob)[xy] - containerBounds[xy];
         }
       }
 
@@ -672,17 +706,21 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
   }
 
   private createKnobElement(knobIndex: number) {
+    const { handleEvent } = this;
     const isAltKnob = knobIndex % 2 ? 'alt-knob' : '';
-    const handleEvent = this as { handleEvent: EventListener };
     return this.createElement(
       html`
         <div
           role="slider"
           part="knob ${isAltKnob} knob-${knobIndex}"
           tabindex="${knobIndex + 1}"
-          @mousedown="${handleEvent}"
-          @touchstart="${handleEvent}"
+          @focus="${handleEvent}"
+          @blur="${handleEvent}"
           @keydown="${handleEvent}"
+          @mousedown="${handleEvent}"
+          @mouseenter="${handleEvent}"
+          @mouseleave="${handleEvent}"
+          @touchstart="${handleEvent}"
         ></div>
       `
     );
@@ -785,6 +823,26 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
     });
   }
 
+  private setActive(active = true, timeout = false) {
+    // Clear all active
+    this.knobIndexes.forEach(i => this.knobElement(i)?.classList.remove('active'));
+    // Set current active
+    if (this.knob) {
+      if (active) {
+        this.knob.classList.add('active');
+        this.tooltip.classList.add('active');
+        if (timeout) {
+          // Debouncer timer
+          clearTimeout(this.tooltipActiveTimeout);
+          this.tooltipActiveTimeout = window.setTimeout(() => this.setActive(false), 1000);
+        }
+      } else if (!(this.knob.matches(':hover') || this.dragging)) {
+        this.knob.classList.remove('active');
+        this.tooltip.classList.remove('active');
+      }
+    }
+  }
+
   private keyMove(event: Event, knobIndex: number) {
     let flag = false;
     const key = (event as KeyboardEvent).key || (event as KeyboardEvent).keyCode;
@@ -820,14 +878,15 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
       event.preventDefault();
       event.stopPropagation();
     }
+    this.setActive(true, true);
   }
 
   private getBounds(el: HTMLElement) {
     return el.getBoundingClientRect();
   }
 
-  private get lineBounds() {
-    return this.getBounds(this.line as HTMLElement);
+  private get containerBounds() {
+    return this.getBounds(this.container as HTMLElement);
   }
 
   private createElement(template: TemplateResult) {
