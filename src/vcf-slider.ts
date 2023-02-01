7d585f4fb65ec71dad043e189219e4453042d541
Nii Yeboah <nii@vaadin.com>
2023-01-31 21:15:00 -0800

    v23.3.3

--- src/vcf-slider.ts
+++ src/vcf-slider.ts
@@ -75,7 +75,7 @@ export class Slider extends CustomEventMixin(ThemableMixin(LitElement)) {
   }
 
   protected static get version() {
-    return '23.3.2';
+    return '23.3.3';
   }
 
   private knob?: HTMLElement;
